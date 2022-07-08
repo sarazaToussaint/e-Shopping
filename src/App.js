8dc8a9994f0c1198a0f7ec8c846b57ad7509fe3b
sarazaToussaint <tmsaraza@gmail.com>
2022-07-08 18:04:57 +0200

    Create the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
