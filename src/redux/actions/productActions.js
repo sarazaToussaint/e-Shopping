0af49d5d7255cadbe1f7b99312dc0c81cf3da156
sarazaToussaint <tmsaraza@gmail.com>
2022-07-08 16:03:42 +0200

    Create actions

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,17 @@
+import { ActionTypes } from "../contents/action-type"
+
+export const setProducts = (products) => {
+  
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products,
+    }
+}
+
+export const selectProduct = (product) => {
+  
+    return {
+        type: ActionTypes.SELECT_PRODUCT,
+        payload: product,
+    }
+}
