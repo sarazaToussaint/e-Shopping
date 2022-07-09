775a14d0cc72dbb836ca7f4a311ca2dd153f25da
sarazaToussaint <tmsaraza@gmail.com>
2022-07-09 13:13:00 +0200

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Route path="/" exact component={ProductListing}></Route>
+        <Route path="/product/:productId" exact component={ProductDetail}></Route>
+        <Route>404 not found</Route>
+      </Router>
     </div>
   );
 }
